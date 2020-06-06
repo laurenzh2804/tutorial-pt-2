104f59bc50db1e4e62d35cd749f79ab0ee0bc212
Laurenz <laurenz@hilgendorf.de>
2020-06-06 16:30:52 +0200

    Setup PM2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ssh -i "ec2-52-15-52-34.us-east-2.compute.amazonaws.com',
+      host: 'ec2-52-15-52-34.us-east-2.compute.amazonaws.com',
       key: '~/.ssh/laurenz-key-pair.pem',
       ref: 'origin/master',
       repo: 'git@github.com:laurenzh2804/tutorial-pt-2.git',

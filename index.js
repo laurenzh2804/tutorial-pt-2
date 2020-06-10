fa2376413f765afae337ffec4b4a4a9b075fd984
Laurenz <laurenz@hilgendorf.de>
2020-06-10 06:52:45 +0200

    Setup PM2

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 const express = require('express');
+const mysql = require('mysql');
 const app = express();
 
+const con = mysql.createConnection({
+	host: "testdb.cbmtnpuqjiek.us-east-2.rds.amazonaws.com",
+	user: "root",
+	password: "Hilgi2209$",
+	database: "testdb"
+});
 
-/*app.get('/', (req, res) => {
+
+app.get('/', function(req, res) {
 	res.send('Hey Maus!');
-});*/
-app.use(express.static('public'));
+	con.connect(function(err) {
+		if(err) throw err;
+		console.log("Connected!");
+		con.query("SELECT name1, street, house_number FROM testdb.suppliers", function(err, result) {
+			if (err) throw err;
+			//console.log("Result: " + result);
+			var reply = "";
+			for(var i = 0; i < result.lenght; i++) {
+				reply += result[i].name1 + " wohnt in " + result[i].street + " " + result[i].house_number + "\n";
+			}
+			res.send(reply);
+		});
+	});
+});
+//app.use(express.static('public'));
 
 app.listen(3000, () => console.log('Server running on port 3000'));
+
+
 
